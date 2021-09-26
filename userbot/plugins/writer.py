924601ff3953467849842a12ae5289cf4ca19645
abasheyari595 <84859238+abasheyari595@users.noreply.github.com>
2021-09-26 23:16:32 +0200

    Update writer.py

--- userbot/plugins/writer.py
+++ userbot/plugins/writer.py
@@ -64,7 +64,7 @@ async def writer(e):
   
 
     data = json.loads('{"NH":"https://github.com/abasheyari595/maker/blob/main/TalkingToTheMoon.ttf?raw=true","CH":"https://github.com/abasheyari595/maker/blob/main/Mumsies.ttf?raw=true","SH":"https://github.com/abasheyari595/maker/blob/main/Always%20In%20My%20Heart.ttf?raw=true","FH":"https://github.com/abasheyari595/maker/blob/main/Husband%20of%20the%20Millennium.ttf?raw=true","عربي":"https://github.com/abasheyari595/maker/blob/main/Gold.ttf?raw=true"}')
-    data_w = json.loads('{"NH":55,"CH":40,"SH":75,"FH":45,"GH":60}') 
+    data_w = json.loads('{"NH":55,"CH":40,"SH":75,"FH":45,"عربي":60}') 
     fontdtyli = f"https://github.com/abasheyari595/maker/blob/main/assfont.ttf?raw=true"
 
     if fnt in ['NH', 'CH', 'SH', 'FH', 'GH']:
