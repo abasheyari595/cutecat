56f02ae39ff3240001b99683772c51c4dcf8acef
abasheyari595 <abasheyari595@gmail.com>
2024-03-05 17:45:26 +0200

    Update __init__.py

--- userbot/helpers/__init__.py
+++ userbot/helpers/__init__.py
@@ -1,5 +1,4 @@
 from . import fonts
-from . import memeshelper as catmemes
 from .aiohttp_helper import AioHttp
 from .utils import *
 
@@ -7,10 +6,8 @@ flag = True
 check = 0
 while flag:
     try:
-        from . import nsfw as useless
         from .chatbot import *
         from .functions import *
-        from .memeifyhelpers import *
         from .progress import *
         from .qhelper import process
         from .tools import *
