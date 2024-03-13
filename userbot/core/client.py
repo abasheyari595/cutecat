1ac4cf211d6f762c30a8e39d85d74e5addbea2e7
abasheyari595 <abasheyari595@gmail.com>
2024-03-13 14:46:27 +0200

    Add files via upload

--- userbot/core/client.py
+++ userbot/core/client.py
@@ -105,11 +105,11 @@ class CatUserBotClient(TelegramClient):
             async def wrapper(check):  # sourcery no-metrics
                 if groups_only and not check.is_group:
                     return await edit_delete(
-                        check, "`I don't think this is a group.`", 10
+                        check, "`لا أعتقد أن هذه مجموعة.`", 10
                     )
                 if private_only and not check.is_private:
                     return await edit_delete(
-                        check, "`I don't think this is a personal Chat.`", 10
+                        check, "`لا أعتقد أن هذه محادثة شخصية.`", 10
                     )
                 try:
                     await func(check)
@@ -118,34 +118,34 @@ class CatUserBotClient(TelegramClient):
                 except KeyboardInterrupt:
                     pass
                 except MessageNotModifiedError:
-                    LOGS.error("Message was same as previous message")
+                    LOGS.error("الرسالة كانت نفس الرسالة السابقة")
                 except MessageIdInvalidError:
-                    LOGS.error("Message was deleted or cant be found")
+                    LOGS.error("تم حذف الرسالة أو لا يمكن العثور عليها")
                 except BotInlineDisabledError:
-                    await edit_delete(check, "`Turn on Inline mode for our bot`", 10)
+                    await edit_delete(check, "`قم بتشغيل وضع الانلاين للبوت من @BotFather`", 10)
                 except ChatSendStickersForbiddenError:
                     await edit_delete(
-                        check, "`I guess i can't send stickers in this chat`", 10
+                        check, "`tأعتقد أنني لا أستطيع إرسال ملصقات في هذه الدردشة`", 10
                     )
                 except BotResponseTimeoutError:
                     await edit_delete(
-                        check, "`The bot didnt answer to your query in time`", 10
+                        check, "`لم يُجب الروبوت على استفسارك في الوقت المناسب`", 10
                     )
                 except ChatSendMediaForbiddenError:
-                    await edit_delete(check, "`You can't send media in this chat`", 10)
+                    await edit_delete(check, "`لا يمكنك إرسال الوسائط في هذه الدردشة`", 10)
                 except AlreadyInConversationError:
                     await edit_delete(
                         check,
-                        "`A conversation is already happening with the given chat. try again after some time.`",
+                        "`هناك محادثة تجري بالفعل مع الدردشة المحددة. حاول مرة أخرى بعد مرور بعض الوقت.`",
                         10,
                     )
                 except ChatSendInlineForbiddenError:
                     await edit_delete(
-                        check, "`You can't send inline messages in this chat.`", 10
+                        check, "`لا يمكنك إرسال رسائل انلاين في هذه الدردشة.`", 10
                     )
                 except FloodWaitError as e:
                     LOGS.error(
-                        f"A flood wait of {e.seconds} occured. wait for {e.seconds} seconds and try"
+                        f"سبام انتظار {e.seconds} حدث. انتظر {e.seconds} ثواني وحاول مرة اخري"
                     )
                     await check.delete()
                     await asyncio.sleep(e.seconds + 5)
@@ -155,22 +155,22 @@ class CatUserBotClient(TelegramClient):
                         if Config.PRIVATE_GROUP_BOT_API_ID == 0:
                             return
                         date = (datetime.datetime.now()).strftime("%m/%d/%Y, %H:%M:%S")
-                        ftext = f"\nDisclaimer:\nThis file is pasted only here ONLY here,\
-                                  \nwe logged only fact of error and date,\nwe respect your privacy,\
-                                  \nyou may not report this error if you've\
-                                  \nany confidential data here, no one will see your data\
-                                  \n\n--------BEGIN USERBOT TRACEBACK LOG--------\
-                                  \nDate: {date}\nGroup ID: {str(check.chat_id)}\
-                                  \nSender ID: {str(check.sender_id)}\
-                                  \nMessage Link: {await check.client.get_msg_link(check)}\
-                                  \n\nEvent Trigger:\n{str(check.text)}\
-                                  \n\nTraceback info:\n{str(traceback.format_exc())}\
-                                  \n\nError text:\n{str(sys.exc_info()[1])}"
+                        ftext = f"\nاخلاء مسئولية:\nيتم لصق هذا الملف هنا فقط,\
+                                  \nلقد سجلنا حقيقة الخطأ والتاريخ فقط,\nنحن نحترم خصوصيتك,\
+                                  \nلا يجوز لك الإبلاغ عن هذا الخطأ إذا كنت قد قمت بذلك\
+                                  \nأي بيانات سرية هنا، لن يرى أحد بياناتك\
+                                  \n\n--------بدء سجل تتبع المستخدم--------\
+                                  \nالتاريخ: {date}\nGroup ID: {str(check.chat_id)}\
+                                  \nايدي المرسل: {str(check.sender_id)}\
+                                  \nرابط الرساله: {await check.client.get_msg_link(check)}\
+                                  \n\nمشغل الحدث:\n{str(check.text)}\
+                                  \n\nمعلومات التتبع:\n{str(traceback.format_exc())}\
+                                  \n\nنص الخطأ:\n{str(sys.exc_info()[1])}"
                         new = {
                             "error": str(sys.exc_info()[1]),
                             "date": datetime.datetime.now(),
                         }
-                        ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+                        ftext += "\n\n--------نهاية سجل تتبع المستخدم--------"
                         command = 'git log --pretty=format:"%an: %s" -5'
                         ftext += "\n\n\nLast 5 commits:\n"
                         output = (await runcmd(command))[:2]
@@ -179,14 +179,14 @@ class CatUserBotClient(TelegramClient):
                         pastelink = await paste_message(
                             ftext, pastetype="s", markdown=False
                         )
-                        text = "**CatUserbot Error report**\n\n"
-                        link = "[here](https://t.me/catuserbot_support)"
-                        text += "If you wanna you can report it"
-                        text += f"- just forward this message {link}.\n"
+                        text = "**تقرير خطأ كات بالعربي**\n\n"
+                        link = "[here](https://t.me/x0ceo)"
+                        text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
+                        text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
                         text += (
-                            "Nothing is logged except the fact of error and date\n\n"
+                            "لا يتم تسجيل أي شيء باستثناء حقيقة الخطأ والتاريخ\n\n"
                         )
-                        text += f"**Error report : ** [{new['error']}]({pastelink})"
+                        text += f"**تقرير الخطأ : ** [{new['error']}]({pastelink})"
                         await check.client.send_message(
                             Config.PRIVATE_GROUP_BOT_API_ID, text, link_preview=False
                         )
@@ -262,9 +262,9 @@ class CatUserBotClient(TelegramClient):
                 except KeyboardInterrupt:
                     pass
                 except MessageNotModifiedError:
-                    LOGS.error("Message was same as previous message")
+                    LOGS.error("الرسالة كانت نفس الرسالة السابقة")
                 except MessageIdInvalidError:
-                    LOGS.error("Message was deleted or cant be found")
+                    LOGS.error("تم حذف الرسالة أو لا يمكن العثور عليها")
                 except BaseException as e:
                     # Check if we have to disable error logging.
                     LOGS.exception(e)  # Log the error in console
@@ -272,22 +272,22 @@ class CatUserBotClient(TelegramClient):
                         if Config.PRIVATE_GROUP_BOT_API_ID == 0:
                             return
                         date = (datetime.datetime.now()).strftime("%m/%d/%Y, %H:%M:%S")
-                        ftext = f"\nDisclaimer:\nThis file is pasted only here ONLY here,\
-                                    \nwe logged only fact of error and date,\nwe respect your privacy,\
-                                    \nyou may not report this error if you've\
-                                    \nany confidential data here, no one will see your data\
-                                    \n\n--------BEGIN USERBOT TRACEBACK LOG--------\
-                                    \nDate: {date}\nGroup ID: {str(check.chat_id)}\
-                                    \nSender ID: {str(check.sender_id)}\
-                                    \nMessage Link: {await check.client.get_msg_link(check)}\
-                                    \n\nEvent Trigger:\n{str(check.text)}\
-                                    \n\nTraceback info:\n{str(traceback.format_exc())}\
-                                    \n\nError text:\n{str(sys.exc_info()[1])}"
+                        ftext = f"\nاخلاء مسئولية:\nيتم لصق هذا الملف هنا فقط,\
+                                  \nلقد سجلنا حقيقة الخطأ والتاريخ فقط,\nنحن نحترم خصوصيتك,\
+                                  \nلا يجوز لك الإبلاغ عن هذا الخطأ إذا كنت قد قمت بذلك\
+                                  \nأي بيانات سرية هنا، لن يرى أحد بياناتك\
+                                  \n\n--------بدء سجل تتبع المستخدم--------\
+                                  \nالتاريخ: {date}\nGroup ID: {str(check.chat_id)}\
+                                  \nايدي المرسل: {str(check.sender_id)}\
+                                  \nرابط الرساله: {await check.client.get_msg_link(check)}\
+                                  \n\nمشغل الحدث:\n{str(check.text)}\
+                                  \n\nمعلومات التتبع:\n{str(traceback.format_exc())}\
+                                  \n\nنص الخطأ:\n{str(sys.exc_info()[1])}"
                         new = {
                             "error": str(sys.exc_info()[1]),
                             "date": datetime.datetime.now(),
                         }
-                        ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+                        ftext += "\n\n--------نهاية سجل تتبع المستخدم--------"
                         command = 'git log --pretty=format:"%an: %s" -5'
                         ftext += "\n\n\nLast 5 commits:\n"
                         output = (await runcmd(command))[:2]
@@ -296,14 +296,14 @@ class CatUserBotClient(TelegramClient):
                         pastelink = await paste_message(
                             ftext, pastetype="s", markdown=False
                         )
-                        text = "**CatUserbot Error report**\n\n"
-                        link = "[here](https://t.me/catuserbot_support)"
-                        text += "If you wanna you can report it"
-                        text += f"- just forward this message {link}.\n"
+                        text = "**تقرير خطأ كات بالعربي**\n\n"
+                        link = "[here](https://t.me/x0ceo)"
+                        text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
+                        text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
                         text += (
-                            "Nothing is logged except the fact of error and date\n\n"
+                            "لا يتم تسجيل أي شيء باستثناء حقيقة الخطأ والتاريخ\n\n"
                         )
-                        text += f"**Error report : ** [{new['error']}]({pastelink})"
+                        text += f"**تقرير الخطأ : ** [{new['error']}]({pastelink})"
                         await check.client.send_message(
                             Config.PRIVATE_GROUP_BOT_API_ID, text, link_preview=False
                         )
@@ -329,7 +329,7 @@ class CatUserBotClient(TelegramClient):
         for _, process in self.running_processes.items():
             try:
                 process.kill()
-                LOGS.debug("Killed %d which was still running.", process.pid)
+                LOGS.debug("تم قتل %d الذي كان لا يزال قيد التشغيل.", process.pid)
             except Exception as e:
                 LOGS.debug(e)
         self.running_processes.clear()
