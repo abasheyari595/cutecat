d3d7bbd938dc57549220cecb4e12f98fedeb13c3
abasheyari595 <abasheyari595@gmail.com>
2024-03-13 17:41:00 +0200

    Add files via upload

--- userbot/plugins/ايدي.py
+++ userbot/plugins/ايدي.py
@@ -14,44 +14,62 @@ from userbot.core.logger import logging
 from ..Config import Config
 from ..core.managers import edit_or_reply
 from ..helpers import get_user_from_event, reply_id
-from . import spamwatch
 
 plugin_category = "utils"
 LOGS = logging.getLogger(__name__)
 
 
+async def get_user_from_event(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_object = await event.client.get_entity(previous_message.sender_id)
+    else:
+        user = event.pattern_match.group(1)
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        if isinstance(user, int) or user.startswith("@"):
+            user_obj = await event.client.get_entity(user)
+            return user_obj
+        try:
+            user_object = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_object
+
+
 async def fetch_info(replied_user, event):
-    """الحصول على تفاصيل من ايدي المستخدم."""
+    """Get details from the User object."""
+    FullUser = (await event.client(GetFullUserRequest(replied_user.id))).full_user
     replied_user_profile_photos = await event.client(
-        GetUserPhotosRequest(
-            user_id=replied_user.user.id, offset=42, max_id=0, limit=80
-        )
-    )
-    replied_user_profile_photos_count = "لم يقم المستخدم بتعيين صورة الملف الشخصي"
+        GetUserPhotosRequest(user_id=replied_user.id, offset=42, max_id=0, limit=80)    )
+    replied_user_profile_photos_count = "لايـوجـد بروفـايـل"
+    dc_id = "Can't get dc id"
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
+        dc_id = replied_user.photo.dc_id
     except AttributeError:
         pass
-    user_id = replied_user.user.id
-    first_name = replied_user.user.first_name
-    last_name = replied_user.user.last_name
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except Exception:
-        dc_id = "تعذر جلب معرف DC"
-    common_chat = replied_user.common_chats_count
-    username = replied_user.user.username
-    user_bio = replied_user.about
-    is_bot = replied_user.user.bot
-    restricted = replied_user.user.restricted
-    verified = replied_user.user.verified
-    photo = await event.client.download_profile_photo(
-        user_id,
-        Config.TMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg",
-        download_big=True,
-    )
-    first_name = (
-        first_name.replace("\u2060", "")
+    user_id = replied_user.id
+    first_name = replied_user.first_name
+    full_name = FullUser.private_forward_name
+    common_chat = FullUser.common_chats_count
+    username = replied_user.username
+    user_bio = FullUser.about
+    is_bot = replied_user.bot
+    restricted = replied_user.restricted
+    verified = replied_user.verified
+    photo = await event.client.download_profile_photo(     user_id,     Config.TMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg",    download_big=True  )
+    first_name = (      first_name.replace("\u2060", "")
         if first_name
         else ("هذا المستخدم ليس له اسم")
     )
@@ -69,75 +87,6 @@ async def fetch_info(replied_user, event):
     caption += f'• ⚜️ | <a href="tg://user?id={user_id}">{first_name}</a> \n'
     return photo, caption
 
-
-@catub.cat_cmd(
-    pattern="^كشف(?:\s|$)([\s\S]*)",
-    command=("كشف", plugin_category),
-    info={
-        "header": "يحصل على معلومات من مستخدم مثل قيود حظر spamwatch أو CAS.",
-        "description": "وهذا يشبه ما إذا كان محظور هو SPAMWATCH أو CAS والمعلومات الصغيرة مثل المجموعات المشتركة، DC ..etc.",
-        "usage": "{tr}كشف <إسم المستخـدم/معرّف المستخـدم/بالرد>",
-    },
-)
-async def _(event):
-    "يحصل على معلومات من مستخدم مثل القيود التي يحظرها SPAMWatch أو CAS"
-    replied_user, error_i_a = await get_user_from_event(event)
-    if not replied_user:
-        return
-    catevent = await edit_or_reply(event, "⌔︙ جار إحضار معلومات المستخدم اننظر قليلا ⚒️")
-    replied_user = await event.client(GetFullUserRequest(replied_user.id))
-    user_id = replied_user.user.id
-    # some people have weird HTML in their names
-    first_name = html.escape(replied_user.user.first_name)
-    # https://stackoverflow.com/a/5072031/4723940
-    # some Deleted Accounts do not have first_name
-    if first_name is not None:
-        # some weird people (like me) have more than 4096 characters in their
-        # names
-        first_name = first_name.replace("\u2060", "")
-    # inspired by https://telegram.dog/afsaI181
-    common_chats = replied_user.common_chats_count
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except Exception:
-        dc_id = "Couldn't fetch DC ID!"
-    if spamwatch:
-        ban = spamwatch.get_ban(user_id)
-        if ban:
-            sw = f"**Spamwatch Banned :** `True` \n       **-**🤷‍♂️**Reason : **`{ban.reason}`"
-        else:
-            sw = f"**Spamwatch Banned :** `False`"
-    else:
-        sw = "**Spamwatch Banned :**`Not Connected`"
-    try:
-        casurl = "https://api.cas.chat/check?user_id={}".format(user_id)
-        data = get(casurl).json()
-    except Exception as e:
-        LOGS.info(e)
-        data = None
-    if data:
-        if data["ok"]:
-            cas = "**Antispam(CAS) Banned :** `True`"
-        else:
-            cas = "**Antispam(CAS) Banned :** `False`"
-    else:
-        cas = "**Antispam(CAS) Banned :** `Couldn't Fetch`"
-    caption = """**معلومات المسـتخدم [{}](tg://user?id={}) :
-   ⌔︙⚕️ الايدي: `{}`
-   ⌔︙👥 المجموعات المشتركه : `{}`
-   ⌔︙🌏 رقم قاعده البيانات : `{}`
-   ⌔︙🔏 هل هو حساب موثق  : `{}`
-""".format(
-        first_name,
-        user_id,
-        user_id,
-        common_chats,
-        dc_id,
-        replied_user.user.restricted,
-        sw,
-        cas,
-    )
-    await edit_or_reply(catevent, caption)
 
 
 @catub.cat_cmd(
